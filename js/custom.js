3578a5539d7f5f6747a4999536afe83d67e4ab79
balu8722 <balakrishna.g@dollarbirdinc.com>
2022-04-12 20:36:19 +0530

    all sections images are replaced

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,31 @@ $(document).ready(function(){
         nextArrow: '<span class="arrow-right"></span>',
     });
 });
+$(function () {
+  var header = $(".top-header");
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  });
+});
+/* javscrift for counter */
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+  $(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+  }, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+  $(this).text(Math.ceil(now));
+  }
+  });
+  });
+  
+  });
