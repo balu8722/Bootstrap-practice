1602027139964534a0a9a69daa47e1bfbcd9ce23
balu8722 <balakrishna.g@dollarbirdinc.com>
2022-04-13 21:35:49 +0530

    crew slider and footer section added

--- js/custom.js
+++ js/custom.js
@@ -35,4 +35,35 @@ $(document).ready(function() {
   });
   });
   
-  });
+});
+/* javscrift for counter ends */
+  
+  /* javscrift for crew-slider starts */
+$('.crew-slick').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      prevArrow: '<span class="arrow-left"></span>',
+      nextArrow: '<span class="arrow-right"></span>',
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 2
+      }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
